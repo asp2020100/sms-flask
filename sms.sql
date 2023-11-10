15e7e66e46afa7b5074d95eb035f61820a925289
R.M.Y.P.Rathnayake <122531660+asp2020100@users.noreply.github.com>
2023-11-10 14:43:34 +0530

    Update sms.sql

--- sms.sql
+++ sms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 10, 2023 at 04:23 AM
--- Server version: 8.0.35
--- PHP Version: 8.0.26
+-- Generation Time: Nov 10, 2023 at 10:13 AM
+-- Server version: 8.0.34-0ubuntu0.22.04.1
+-- PHP Version: 8.1.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `sms`
 --
+CREATE DATABASE IF NOT EXISTS `sms` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sms`;
 
 -- --------------------------------------------------------
 
@@ -27,21 +29,20 @@ SET time_zone = "+00:00";
 -- Table structure for table `attendence`
 --
 
-DROP TABLE IF EXISTS `attendence`;
-CREATE TABLE IF NOT EXISTS `attendence` (
-  `aid` int NOT NULL AUTO_INCREMENT,
-  `rollno` varchar(20) NOT NULL,
-  `attendance` int NOT NULL,
-  PRIMARY KEY (`aid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `attendence` (
+  `aid` int NOT NULL,
+  `indexno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `attendance` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `attendence`
 --
 
-INSERT INTO `attendence` (`aid`, `rollno`, `attendance`) VALUES
+INSERT INTO `attendence` (`aid`, `indexno`, `attendance`) VALUES
 (6, '1ve17cs012', 98),
-(7, '4840', 100);
+(7, '4840', 100),
+(8, '4772', 80);
 
 -- --------------------------------------------------------
 
@@ -49,12 +50,10 @@ INSERT INTO `attendence` (`aid`, `rollno`, `attendance`) VALUES
 -- Table structure for table `department`
 --
 
-DROP TABLE IF EXISTS `department`;
-CREATE TABLE IF NOT EXISTS `department` (
-  `cid` int NOT NULL AUTO_INCREMENT,
-  `branch` varchar(50) NOT NULL,
-  PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `department` (
+  `cid` int NOT NULL,
+  `branch` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `department`
@@ -74,41 +73,37 @@ INSERT INTO `department` (`cid`, `branch`) VALUES
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE IF NOT EXISTS `student` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `rollno` varchar(20) NOT NULL,
+CREATE TABLE `student` (
+  `id` int NOT NULL,
+  `indexno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `sname` varchar(50) NOT NULL,
   `sem` int NOT NULL,
   `gender` varchar(50) NOT NULL,
   `branch` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `number` varchar(12) NOT NULL,
-  `address` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `rollno`, `sname`, `sem`, `gender`, `branch`, `email`, `number`, `address`) VALUES
-(7, '4840', 'R.M.Y.P.Rathnayake', 4, 'male', 'CS without CS Degree', 'cs@4840.eu.org', '0711234567', 'Anuradhapura');
+INSERT INTO `student` (`id`, `indexno`, `sname`, `sem`, `gender`, `branch`, `email`, `number`, `address`) VALUES
+(7, '4840', 'R.M.Y.P.Rathnayake', 4, 'male', 'CS', 'cs@4840.eu.org', '0711234567', 'Anuradhapura'),
+(8, '4772', 'S.S.B.Senanayaka', 4, 'female', 'Chemistry', 'admin@4772.eu.org', '0771234567', 'Galle');
 
 --
 -- Triggers `student`
 --
-DROP TRIGGER IF EXISTS `DELETE`;
 DELIMITER $$
 CREATE TRIGGER `DELETE` BEFORE DELETE ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,OLD.rollno,'STUDENT DELETED',NOW())
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `Insert`;
 DELIMITER $$
 CREATE TRIGGER `Insert` AFTER INSERT ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.rollno,'STUDENT INSERTED',NOW())
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `UPDATE`;
 DELIMITER $$
 CREATE TRIGGER `UPDATE` AFTER UPDATE ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.rollno,'STUDENT UPDATED',NOW())
 $$
@@ -120,20 +115,11 @@ DELIMITER ;
 -- Table structure for table `test`
 --
 
-DROP TABLE IF EXISTS `test`;
-CREATE TABLE IF NOT EXISTS `test` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `test` (
+  `id` int NOT NULL,
   `name` varchar(52) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `test`
---
-
-INSERT INTO `test` (`id`, `name`, `email`) VALUES
-(1, 'aaa', 'aaa@gmail.com');
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -141,22 +127,22 @@ INSERT INTO `test` (`id`, `name`, `email`) VALUES
 -- Table structure for table `trig`
 --
 
-DROP TABLE IF EXISTS `trig`;
-CREATE TABLE IF NOT EXISTS `trig` (
-  `tid` int NOT NULL AUTO_INCREMENT,
-  `rollno` varchar(50) NOT NULL,
+CREATE TABLE `trig` (
+  `tid` int NOT NULL,
+  `indexno` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `action` varchar(50) NOT NULL,
-  `timestamp` datetime NOT NULL,
-  PRIMARY KEY (`tid`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `trig`
 --
 
-INSERT INTO `trig` (`tid`, `rollno`, `action`, `timestamp`) VALUES
+INSERT INTO `trig` (`tid`, `indexno`, `action`, `timestamp`) VALUES
 (10, '4840', 'STUDENT INSERTED', '2023-11-09 23:39:18'),
-(11, '4840', 'STUDENT UPDATED', '2023-11-09 23:43:45');
+(11, '4840', 'STUDENT UPDATED', '2023-11-09 23:43:45'),
+(12, '4772', 'STUDENT INSERTED', '2023-11-10 04:27:13'),
+(13, '4840', 'STUDENT UPDATED', '2023-11-10 04:28:45');
 
 -- --------------------------------------------------------
 
@@ -164,14 +150,12 @@ INSERT INTO `trig` (`tid`, `rollno`, `action`, `timestamp`) VALUES
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(500) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `user`
@@ -179,6 +163,86 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
 (5, 'yasas', 'sms@4840.eu.org', 'scrypt:32768:8:1$bXTyoLbDpQ4awiBA$77af64820b1ee10a6dfd6130e01bca6f9b51ecf3043fed31325f2c843fb865490175e8b37eeb821f4b76f2a57d4d2bc2c94c456d047257f6a8c7e3ca7f2fc690');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendence`
+--
+ALTER TABLE `attendence`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trig`
+--
+ALTER TABLE `trig`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendence`
+--
+ALTER TABLE `attendence`
+  MODIFY `aid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `trig`
+--
+ALTER TABLE `trig`
+  MODIFY `tid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
