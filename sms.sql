9e73bc84588b2feb03b7509727866e30b4347fd1
R.M.Y.P.Rathnayake <122531660+asp2020100@users.noreply.github.com>
2023-11-10 06:39:57 +0530

    db added

---
+++ sms.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 10, 2023 at 01:09 AM
+-- Server version: 8.0.35
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendence`
+--
+
+DROP TABLE IF EXISTS `attendence`;
+CREATE TABLE IF NOT EXISTS `attendence` (
+  `aid` int NOT NULL AUTO_INCREMENT,
+  `rollno` varchar(20) NOT NULL,
+  `attendance` int NOT NULL,
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `attendence`
+--
+
+INSERT INTO `attendence` (`aid`, `rollno`, `attendance`) VALUES
+(6, '1ve17cs012', 98),
+(7, '4840', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE IF NOT EXISTS `department` (
+  `cid` int NOT NULL AUTO_INCREMENT,
+  `branch` varchar(50) NOT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`cid`, `branch`) VALUES
+(9, 'CS'),
+(10, 'Maths'),
+(11, 'Physics'),
+(12, 'Bio'),
+(13, 'HPT'),
+(14, 'Chemistry');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rollno` varchar(20) NOT NULL,
+  `sname` varchar(50) NOT NULL,
+  `sem` int NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `address` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `rollno`, `sname`, `sem`, `gender`, `branch`, `email`, `number`, `address`) VALUES
+(7, '4840', 'R.M.Y.P.Rathnayake', 4, 'male', 'CS without CS Degree', 'cs@4840.eu.org', '0711234567', 'Anuradhapura');
+
+--
+-- Triggers `student`
+--
+DROP TRIGGER IF EXISTS `DELETE`;
+DELIMITER $$
+CREATE TRIGGER `DELETE` BEFORE DELETE ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,OLD.rollno,'STUDENT DELETED',NOW())
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `Insert`;
+DELIMITER $$
+CREATE TRIGGER `Insert` AFTER INSERT ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.rollno,'STUDENT INSERTED',NOW())
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `UPDATE`;
+DELIMITER $$
+CREATE TRIGGER `UPDATE` AFTER UPDATE ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.rollno,'STUDENT UPDATED',NOW())
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE IF NOT EXISTS `test` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(52) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `name`, `email`) VALUES
+(1, 'aaa', 'aaa@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trig`
+--
+
+DROP TABLE IF EXISTS `trig`;
+CREATE TABLE IF NOT EXISTS `trig` (
+  `tid` int NOT NULL AUTO_INCREMENT,
+  `rollno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `trig`
+--
+
+INSERT INTO `trig` (`tid`, `rollno`, `action`, `timestamp`) VALUES
+(10, '4840', 'STUDENT INSERTED', '2023-11-09 23:39:18'),
+(11, '4840', 'STUDENT UPDATED', '2023-11-09 23:43:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
+(5, 'yasas', 'sms@4840.eu.org', 'scrypt:32768:8:1$bXTyoLbDpQ4awiBA$77af64820b1ee10a6dfd6130e01bca6f9b51ecf3043fed31325f2c843fb865490175e8b37eeb821f4b76f2a57d4d2bc2c94c456d047257f6a8c7e3ca7f2fc690');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
